e77ac1be5b9e701ad3d8d845934b30cb332a0a63
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-04-26 02:11:57 +0600

    add animations on review section

--- src/Pages/Home/Reviews/Reviews.jsx
+++ src/Pages/Home/Reviews/Reviews.jsx
@@ -1,20 +1,26 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import SingleReview from "./SingleReview";
+import Skeleton from "../../../Components/Skeleton/Skeleton";
 
 const Reviews = () => {
   const [reviews, setreviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:5000/reviews")
       .then((response) => response.json())
-      .then((data) => setreviews(data))
+      .then((data) => {
+        setreviews(data);
+        setIsLoading(false);
+      })
       .catch((error) => console.error(error));
   }, []);
   return (
     <>
       <SectionTitle heading="Reviews"></SectionTitle>
       <div className="grid gap-10 grid-cols-1 lg:grid-cols-3 px-[8%] py-[3%] ">
+        {isLoading && <Skeleton number={3}></Skeleton>}
         {reviews.map((rev) => (
           <SingleReview key={rev._id} rev={rev}></SingleReview>
         ))}
--- src/Pages/Home/Reviews/SingleReview.jsx
+++ src/Pages/Home/Reviews/SingleReview.jsx
@@ -1,16 +1,20 @@
+import FadeAnimations from "../../../Components/Animations/FadeAnimations";
+
 /* eslint-disable react/prop-types */
 const SingleReview = ({ rev }) => {
   const { image, name, review } = rev;
   return (
-    <div className="border border-gray-400 p-5 text-center">
-      <img
-        src={image}
-        alt=""
-        className="mx-auto h-[80px] w-[80px] rounded-full mb-3"
-      />
-      <p className="font-semibold text-lg mb-5">{name}</p>
-      <p className="text-gray-600">{review}</p>
-    </div>
+    <FadeAnimations direction="up" once={false} delay={0.4} duration={0.4}>
+      <div className="border border-gray-400 p-5 text-center">
+        <img
+          src={image}
+          alt=""
+          className="mx-auto h-[80px] w-[80px] rounded-full mb-3"
+        />
+        <p className="font-semibold text-lg mb-5">{name}</p>
+        <p className="text-gray-600">{review}</p>
+      </div>
+    </FadeAnimations>
   );
 };
 
