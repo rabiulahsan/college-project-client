9681f0fefeb9795169794bd351ad6436bb4d0fc1
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-04-26 12:16:33 +0600

    profile image created in profile page

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,10 +1,24 @@
+import UseAuth from "../../Hook/UseAuth";
 import Footer from "../../Shared/Footer/Footer";
 import Navbar from "../../Shared/Navbar/Navbar";
 
 const Profile = () => {
+  const { user } = UseAuth();
+  console.log(user);
   return (
     <div>
       <Navbar></Navbar>
+      <div className="my-[5%]">
+        <img
+          src={user?.photoURL}
+          className="h-[120px] w-[120px] rounded-full mx-auto"
+          alt=""
+        />
+      </div>
+      <div className="">
+        <div className=""></div>
+        <div className=""></div>
+      </div>
       <Footer></Footer>
     </div>
   );
